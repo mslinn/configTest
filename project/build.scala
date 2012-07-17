cd8f95deff342e0d1b3d5fe79432df157398f8c1
Mike Slinn <mslinn@micronauticsresearch.com>
2012-07-17 13:08:10 -0700

    Oh, so close

--- project/build.scala
+++ project/build.scala
@@ -9,22 +9,12 @@ object Playpen extends Build {
 
   configUrl := "https://raw.github.com/Bookish/config/master/scalaBuild/Build.conf"
   //configUrl := SbtDependencies.jarUrl
-  println("Playpen: configUrl=" + configUrl) // how to display this?
+  println(SbtDependencies.settings)
 
-  // Retrieve the Lookup object from versionsTask - does not work! What is the right way of doing this?
-  print("Playpen: versionsTask value=");      configVersionsLookup    map { (lookup: Lookup) => println(lookup) }
-  print("\nPlaypen: credentialsTask value="); configCredentialsLookup map { (lookup: Lookup) => println(lookup) }
-  print("\nPlaypen: serversTask value=");     configServersLookup     map { (lookup: Lookup) => println(lookup) }
-  println
-
-  println("configVersionsLookup.get(\"bkshDomainBus\")" + configVersionsLookup.key.get("bkshDomainBus"))
-
-  // How can I obtain the Lookup instances from the plug-in?!?!?!
-
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := Organization,
-    //version      := configVersionsLookup.key.get("bkshDomainBus"), // does not compile; I would prefer to write V.bkshDomainBus
-    //scalaVersion := configVersionsLookup.key.get("scalaVersion"),  // does not compile; I would prefer to write V.scalaVersion
+  lazy val buildSettings = Defaults.defaultSettings ++ SbtDependencies.settings ++ Seq(
+//    organization := V.Organization,
+//    version      := V.bkshDomainBus,
+//    scalaVersion := V.scala, // V.scala is set by the plug-in, if the config file defines it properly
     crossPaths   := false
   )
 
@@ -37,23 +27,22 @@ object Playpen extends Build {
     scalacOptions ++= Seq("-encoding", "UTF-8", "-deprecation", "-unchecked"),
     javacOptions  ++= Seq("-Xlint:deprecation", "-Xcheckinit"),
     logLevel in compile := Level.Warn,
-    //credentials += Credentials("Artifactory Realm", "ci-sb-1.obi.int", credentials.userid, credentials.password),
-    //publishTo <<= (version) { version: String =>
-      //if (version.trim.endsWith("SNAPSHOT"))
-        //Some("bookish" at servers.artifactory + "libs-snapshot-local/")
-      //else
-        //Some("bookish" at servers.artifactory + "libs-release-local/")
-    //},
+//    credentials += Credentials("Artifactory Realm", "ci-sb-1.obi.int", creds.userid, creds.password), // creds._ set by plug-in
+//    publishTo <<= (version) { version: String =>
+//      if (version.trim.endsWith("SNAPSHOT"))
+//        Some("bookish" at servers.artifactory + "libs-snapshot-local/") // servers._ set by plug-in
+//      else
+//        Some("bookish" at servers.artifactory + "libs-release-local/")
+//    },
     initialCommands +=
       """
         |println("Type :load repl")
       """.stripMargin
   )
 
-  import com.micronautics.dependencyReport.DependencyReport._
   lazy val DomainBus = Project(
     id = "playpen",
     base = file("."),
-    settings = defaultSettings ++ SbtDependencies.settings //++ /*dependencyReportSettings ++*/ /*SbtDependencies ++*/ Seq(libraryDependencies)
+    settings = defaultSettings ++ SbtDependencies.settings
   )
 }
